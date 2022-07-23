d27d78d671e47975c125c83ba7e71ce270c65a03
Amish Menon <amishmenon@gmail.com>
2022-07-23 15:51:00 -0500

    Updates

---
+++ precache-manifest.7c89e7764ee9453602f988e873016c7e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b759742dd02cc53b3f2",
+    "url": "/blockchain-fitness-voting/static/css/main.e363a452.chunk.css"
+  },
+  {
+    "revision": "2b759742dd02cc53b3f2",
+    "url": "/blockchain-fitness-voting/static/js/main.08d6d8c3.chunk.js"
+  },
+  {
+    "revision": "7b8189739769728ec724",
+    "url": "/blockchain-fitness-voting/static/js/runtime~main.d2201473.js"
+  },
+  {
+    "revision": "ce45bfd9a62e29f1010e",
+    "url": "/blockchain-fitness-voting/static/css/2.e1313bcf.chunk.css"
+  },
+  {
+    "revision": "ce45bfd9a62e29f1010e",
+    "url": "/blockchain-fitness-voting/static/js/2.cc7c0d5f.chunk.js"
+  },
+  {
+    "revision": "d85eef0188cca89aae077d68b8d4c41e",
+    "url": "/blockchain-fitness-voting/static/media/bg-1.d85eef01.jpeg"
+  },
+  {
+    "revision": "b3731764e02045f2543e260f69cf2a65",
+    "url": "/blockchain-fitness-voting/static/media/weightlifting-gif.b3731764.gif"
+  },
+  {
+    "revision": "b92a3bff4046517eb5f106b3e4109969",
+    "url": "/blockchain-fitness-voting/static/media/cardio2-gif.b92a3bff.gif"
+  },
+  {
+    "revision": "8bebe53a3e1fb4d300080124956c79d4",
+    "url": "/blockchain-fitness-voting/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/blockchain-fitness-voting/precache-manifest.1285a8c59635c72cd3a794e6330c2c7e.js"
+  "/blockchain-fitness-voting/precache-manifest.7c89e7764ee9453602f988e873016c7e.js"
 );
 
 workbox.clientsClaim();
